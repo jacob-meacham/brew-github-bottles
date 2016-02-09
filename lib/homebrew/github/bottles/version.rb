6bed2dc10880f9096c18b937bf0adc5a8c826587
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-02-08 19:02:08 -0800

    Version 0.2.0.

--- lib/homebrew/github/bottles/version.rb
+++ lib/homebrew/github/bottles/version.rb
@@ -3,7 +3,7 @@ module Homebrew
         module Bottles
             #TODO: A version that pulls from git
             #VERSION = `git describe`.tr! '-', '.'
-            VERSION = '0.1.0.dev'
+            VERSION = '0.2.0'
         end
     end
 end
